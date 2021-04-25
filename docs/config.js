c9bbf5e972868a7cd32a0543837b63a41b322bbb
Hidenori <hfu@users.noreply.github.com>
2021-04-25 12:00:50 +0900

    Create config.js

---
+++ docs/config.js
@@ -0,0 +1,54 @@
+var config = {
+  "accessToken": "pk.eyJ1IjoiaGZ1IiwiYSI6ImlRSGJVUTAifQ.rTx380smyvPc1gUfZv1cmw",
+  "byline": "by the United Nations Vector Tile Toolkit community",
+  "chapters": [
+    { // #17.16/36.085526/140.111088/105.6/50
+      "alignment": "right",
+      "callback": "''",
+      "title": "中央公園",
+      "description": "TXつくば駅の出口A2をのぼると、すぐそこにあるのが<a href='https://www.city.tsukuba.lg.jp/shisetsu/kouensports/kouen/1002823.html'>中央公園</a>です。",
+      "hidden": false,
+      "id": "s1",
+      "location": {
+        "bearing": 105.6,
+        "center": [
+          140.111088,
+          36.085526
+        ],
+        "pitch": 50,
+        "zoom": 17.16
+      },
+      "mapAnimation": "flyTo",
+      "onChapterEnter": [],
+      "onChapterExit": [],
+      "rotateAnimation": true
+    },
+    { // #17.78/36.060907/140.121608/76.9/73
+      "alignment": "right",
+      "callback": "''",
+      "title": "洞峰公園",
+      "description": "<a href='http://tsud.co.jp/dohopark/'>洞峰公園</a>は、茨城県都市公園の一つです。",
+      "hidden": false,
+      "id": "s2",
+      "location": {
+        "bearing": 76.9,
+        "center": [
+          140.121608,
+          36.060907
+        ],
+        "pitch": 73,
+        "zoom": 17.78
+      },
+      "mapAnimation": "flyTo",
+      "onChapterEnter": [],
+      "onChapterExit": [],
+      "rotateAnimation": true
+    }
+  ],
+  "footer": "このプロジェクトは、<a href='https://github.com/optgeo'>Adopt Geodata プロジェクト</a>の一環です。",
+  "showMarkers": false,
+  "style": "https://optgeo.github.io/fgd-tkb/style.json",
+  "subtitle": "つくば市研究学園都市内の公園",
+  "theme": "light",
+  "title": "Tsukuba Parks"
+}
